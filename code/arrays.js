09bac25681caaeda17083e6b911a0bd6dad7ec6c
cbattenplowright <callumbp15@gmail.com>
2023-08-21 14:40:23 +0100

    Q3 from arrays completed

--- code/arrays.js
+++ code/arrays.js
@@ -29,9 +29,8 @@ console.log(stations);
  * How many stations are on the line?
  */
 
-stationCount = null;
-
-// console.log("There are " + stationCount + " stations.");
+stationCount = stations.length;
+console.log("There are " + stationCount + " stations.");
 
 
 
